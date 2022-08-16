90b7cb62b76f3c535d2f9ee9e7cd38352859b0ba
etchichuryg <ginettetchichury@gmail.com>
2022-08-16 23:38:15 +0200

    Auth

--- src/main/java/com/alkemy/challenge/controller/AuthController.java
+++ src/main/java/com/alkemy/challenge/controller/AuthController.java
@@ -1,21 +1,15 @@
 package com.alkemy.challenge.controller;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.*;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.alkemy.challenge.dto.LoginRequestDTO;
 import com.alkemy.challenge.dto.RegisterRequestDTO;
-import com.alkemy.challenge.model.User;
-import com.alkemy.challenge.model.UserRole;
 import com.alkemy.challenge.repository.UserRepository;
 import com.alkemy.challenge.security.util.JwtTokenUtil;
 import com.alkemy.challenge.service.JwtUserDetailsService;
@@ -39,39 +33,7 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public ResponseEntity<?> loginUser(@RequestBody LoginRequestDTO loginRequestDTO) {
-		Map<String, Object> responseMap = new HashMap<>();
-		try {
-		User user = userRepository.findUserByEmail(loginRequestDTO.getEmail());
-		Boolean isValid = BCrypt.checkpw(loginRequestDTO.getPassword()  ,user.getPassword());
-		if(isValid) {
-		UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequestDTO.getEmail());
-		String token = jwtTokenUtil.generateToken(userDetails);
-		responseMap.put("error", false);
-		responseMap.put("email", loginRequestDTO.getEmail());
-		responseMap.put("message", "Account created successfully");
-		responseMap.put("token", token);
-		return ResponseEntity.ok(responseMap);
-
-		}else {
-				responseMap.put("error", true);
-				responseMap.put("message", "Invalid Credentials");
-				return ResponseEntity.status(401).body(responseMap);
-			}
-		} catch (DisabledException e) {
-			e.printStackTrace();
-			responseMap.put("error", true);
-			responseMap.put("message", "User is disabled");
-			return ResponseEntity.status(500).body(responseMap);
-		} catch (BadCredentialsException e) {
-			responseMap.put("error", true);
-			responseMap.put("message", "Invalid Credentials");
-			return ResponseEntity.status(401).body(responseMap);
-		} catch (Exception e) {
-			e.printStackTrace();
-			responseMap.put("error", true);
-			responseMap.put("message", "Something went wrong");
-			return ResponseEntity.status(500).body(responseMap);
-		}
+			return userService.loginUser(loginRequestDTO);
 	}
 
 	@PostMapping("/register")
